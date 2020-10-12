c8987c35ffafd5c17f7001e48f678ae3180c4a5a
Renaud Duret <contact@renaudduret.fr>
2020-10-12 12:58:30 +0200

    Added logging options

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ import requests
 
 # LOGGING CONFIGURATION
 
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 # Functions
 
