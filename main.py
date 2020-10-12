888f3e299c283dc8372b354079b618f0d4419c0a
Renaud Duret <contact@renaudduret.fr>
2020-10-12 09:34:53 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+# bot.py
+import os
+import discord
+import logging
+import platform
+import subprocess
+from discord.ext import commands
+from dotenv import load_dotenv
+import pydig
+import re
+import json
+import requests
+
+# LOGGING CONFIGURATION
+
+logging.basicConfig(level=logging.INFO)
+
+# Functions
+
+# DNS FUNCTIONS
+
+def get_ip(domain_name, dns_type):
+    result = pydig.query(domain_name,dns_type)
+    return(result[0])
+
+# PING FUNCTIONS
+
+def ping_command(host):
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+    command = ['ping', param, '1', host]
+    return subprocess.call(command) == 0
+
+def ping_func(ip):
+   regex = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+   if regex.match(ip):
+       ping_result = ping_command(ip)
+       if ping_result:
+           return('OK')
+       else:
+           return('KO')
+   else:
+       return('The IP given ({}) is not correct, you should pass an IP.'.format(ip))
+
+# HTTP STATUS
+
+def http_status(value):
+    status_request = requests.get(value)
+    return('Status : {}'.format(status_request.status_code))
+
+# bot definition
+bot = commands.Bot(command_prefix='$')
+
+# commands
+@bot.command()
+async def dns(ctx, arg1, arg2):
+    result = get_ip(arg1, arg2)
+    await ctx.send(result)
+
+@bot.command()
+async def ping(ctx, arg):
+    result = ping_func(arg)
+    await ctx.send(result)
+
+@bot.command()
+async def http(ctx, arg):
+    result = http_status(arg)
+    await ctx.send(result)
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot.run(TOKEN)
+
